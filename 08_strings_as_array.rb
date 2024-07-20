4ced8185708785d2400dc701b349dfb4e4883cbe
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-07-20 14:00:32 +0500

    comment added

--- 08_strings_as_array.rb
+++ 08_strings_as_array.rb
@@ -18,4 +18,4 @@ puts "\n"
 puts new_string.split(" ")
 
 puts new_string.gsub('a', 'u') # replaces all valid characters
-puts new_string.sub('a', 'u')
+puts new_string.sub('a', 'u') # replaces the first valid character
