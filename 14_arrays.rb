242dc73292df7cddfa8e2680c11f1cf5ca06e4fd
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-08-01 18:55:43 +0500

    comment added

--- 14_arrays.rb
+++ 14_arrays.rb
@@ -13,4 +13,4 @@ array_1 = [0, 1.0, "Two", false] # Literal array
 # indexes  0  1     2       3
 puts array_1
 
-array_2 = Array.new(5, "Subscribe")
+array_2 = Array.new(5, "Subscribe") # Class array
