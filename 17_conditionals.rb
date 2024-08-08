ac3029c54f4e76cdbd16c07cd12a7931e9b3e11c
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-08-08 22:12:54 +0500

    comment added

--- 17_conditionals.rb
+++ 17_conditionals.rb
@@ -28,5 +28,5 @@ puts "\n\n"
 capital_city = "New Delhi"
 
 unless capital_city == "New Delhi" # false
-  puts "Not the capital of India!"
+  puts "Not the capital of India!" # this will work if false
 end
