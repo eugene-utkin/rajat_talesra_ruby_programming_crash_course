8f80818d3aab7b6e60b0ccc528fa65350b9cceb5
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-08-12 18:27:52 +0500

    fourth then added

--- 18_switch_case.rb
+++ 18_switch_case.rb
@@ -19,5 +19,5 @@ case greeting
   when "English" then puts "Hello!"
   when "French" then puts "Bonjour!"
   when "German" then puts "Guten Tag!"
-  when "Finnish"
+  when "Finnish" then puts "Haloo!"
 end
