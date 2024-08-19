ec4fd63ccf6a7f7d3544130cf1037514269381ad
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-08-19 22:04:04 +0500

    comment added

--- 21_hash.rb
+++ 21_hash.rb
@@ -12,4 +12,4 @@ my_hash = Hash.new(0)
 my_hash["hello"] = "Rajat"
 my_hash[:age] = 100
 my_hash[:language] = "Ruby"
-my_hash[5] = "Count"
+my_hash[5] = "Count" # Here 5 is key and not index. Index doesn't work in hash
