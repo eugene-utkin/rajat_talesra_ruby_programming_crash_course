8275dbc485c779c8f3f583c836f43e20f49e77bc
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-09-02 22:22:56 +0500

    fourth comment added

--- 26_blocks.rb
+++ 26_blocks.rb
@@ -4,4 +4,5 @@
   Blocks are like methods without a name, they are anonymous functions.
   Allows to write powerful flexible code.
   A block is a piece of code that accepts arguments, and returns a value.
+  A block is always passed to a method call.
 =end
