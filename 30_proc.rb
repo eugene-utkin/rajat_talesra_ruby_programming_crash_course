92bd5a0ff5d7b377bf18597b4734ddbcc56f836e
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-09-09 21:18:56 +0500

    fifrth comment added

--- 30_proc.rb
+++ 30_proc.rb
@@ -5,4 +5,5 @@
   - Unlike blocks, procs can be called over and over
   - They can be assigned to variables and passed around
   - Procs can be called explicitly using the call method
+  - Called using `&
 =end
