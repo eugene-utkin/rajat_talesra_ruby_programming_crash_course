649e3bd6ac75a6d4e529f85ba3c6d643a769ec92
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-09-09 21:18:38 +0500

    fourth comment added

--- 30_proc.rb
+++ 30_proc.rb
@@ -4,4 +4,5 @@
   - Procs are like 'saved' blocks
   - Unlike blocks, procs can be called over and over
   - They can be assigned to variables and passed around
+  - Procs can be called explicitly using the call method
 =end
